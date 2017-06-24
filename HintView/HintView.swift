422f5ae0cea03520892dbd145eb6a05d11408d0f
Myron MacBook Pro <hmb87381041@163.com>
2017-06-24 20:01:50 +0800

    update

---
+++ HintView/HintView.swift
@@ -0,0 +1,21 @@
+//
+//  HintView.swift
+//  HintViewDemo
+//
+//  Created by 黄穆斌 on 2017/6/20.
+//  Copyright © 2017年 myron. All rights reserved.
+//
+
+import UIKit
+
+class HintView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
